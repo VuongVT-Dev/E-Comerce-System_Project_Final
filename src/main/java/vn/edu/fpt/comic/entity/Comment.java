0a8f85966d82a07f47044ef22c843fb88fb1be59
Sabageto <tanan0917025981@gmail.com>
2026-03-19 00:59:05 +0700

    update order and comment Entiy

---
+++ src/main/java/vn/edu/fpt/comic/entity/Comment.java
@@ -0,0 +1,38 @@
+package vn.edu.fpt.comic.entity;
+
+public class Comment {
+    import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Entity
+    public class Comment {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+
+        @ManyToOne
+        @JoinColumn(name = "user_id")
+        private User user;
+
+        @ManyToOne
+        @JoinColumn(name = "book_id")
+        private Book book;
+
+        @OneToOne
+        @JoinColumn(name = "order_detail_id")
+        private OrderDetail orderDetail;
+
+        private Integer star;
+        private String content;
+        private Date created_at;
+        private Date updated_at;
+    }
+
+}

5f7d49e77c24a3c2240f2f6691fe0759e6338a47
datdattran7075 <sihira7075@gmail.com>
2026-03-18 22:14:41 +0700

    change user

--- src/main/java/vn/edu/fpt/comic/entity/User.java
+++ src/main/java/vn/edu/fpt/comic/entity/User.java
@@ -1,4 +1,34 @@
 package vn.edu.fpt.comic.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "[user]")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "account_id", referencedColumnName = "id")
+    private Account account;
+
+    private String name;
+    private String address;
+    private String phone;
+
+    @OneToMany(mappedBy = "user")
+    public List<Order> orderList;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    public List<Comment> commentList;
 }

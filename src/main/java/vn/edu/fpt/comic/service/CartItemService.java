9489defc3cf404b69dde4404cf9005b1915b78ac
datdattran7075 <sihira7075@gmail.com>
2026-03-19 11:30:23 +0700

    methof carrt

--- src/main/java/vn/edu/fpt/comic/service/CartItemService.java
+++ src/main/java/vn/edu/fpt/comic/service/CartItemService.java
@@ -4,6 +4,7 @@ package vn.edu.fpt.comic.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import vn.edu.fpt.comic.entity.Book;
 import vn.edu.fpt.comic.entity.CartItem;
 import vn.edu.fpt.comic.entity.User;
 import vn.edu.fpt.comic.repository.CartItemRepository;
@@ -28,5 +29,36 @@ public class CartItemService {
     public List<CartItem> getCartItems(User user) {
         return cartItemRepository.findByUser(user);
     }
+
+    /**
+     * Thêm sách vào giỏ hàng
+     */
+    @Transactional
+    public CartItem addBookToCart(User user, Integer bookId, Integer quantity) {
+        Book book = bookService.findById(bookId);
+
+        if (book == null) {
+            throw new IllegalArgumentException("Book not found!");
+        }
+
+        Optional<CartItem> existingItemOpt = cartItemRepository.findByUserAndBook(user, book);
+
+        if (existingItemOpt.isPresent()) {
+            CartItem existingItem = existingItemOpt.get();
+            existingItem.setQuantity(existingItem.getQuantity() + quantity);
+            existingItem.setUpdatedAt(new Date());
+            return cartItemRepository.save(existingItem);
+        } else {
+            // Tạo CartItem mới
+            CartItem newItem = new CartItem();
+            newItem.setUser(user);
+            newItem.setBook(book);
+            newItem.setQuantity(quantity);
+            newItem.setCreatedAt(new Date());
+            newItem.setUpdatedAt(new Date());
+            return cartItemRepository.save(newItem);
+        }
+    }
+
 
 }

7fc58b9f71bdd8edf82829ffae3a3a77f6286017
datdattran7075 <sihira7075@gmail.com>
2026-03-19 10:56:49 +0700

    service cart

---
+++ src/main/java/vn/edu/fpt/comic/service/CartItemService.java
@@ -0,0 +1,22 @@
+package vn.edu.fpt.comic.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import vn.edu.fpt.comic.repository.CartItemRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CartItemService {
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Autowired
+    private BookService bookService;
+
+}

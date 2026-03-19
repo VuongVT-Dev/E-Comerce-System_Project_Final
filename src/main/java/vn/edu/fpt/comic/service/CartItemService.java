84bb590b27b6594c51149aa6a8e81eb4c2df5e0e
datdattran7075 <sihira7075@gmail.com>
2026-03-19 11:09:30 +0700

    cart se

--- src/main/java/vn/edu/fpt/comic/service/CartItemService.java
+++ src/main/java/vn/edu/fpt/comic/service/CartItemService.java
@@ -4,6 +4,8 @@ package vn.edu.fpt.comic.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import vn.edu.fpt.comic.entity.CartItem;
+import vn.edu.fpt.comic.entity.User;
 import vn.edu.fpt.comic.repository.CartItemRepository;
 
 import java.util.Date;
@@ -18,5 +20,13 @@ public class CartItemService {
 
     @Autowired
     private BookService bookService;
+
+
+    /**
+     * Lấy tất cả CartItem của user
+     */
+    public List<CartItem> getCartItems(User user) {
+        return cartItemRepository.findByUser(user);
+    }
 
 }

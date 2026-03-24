082938c7ee078bc2e496604a8dd9045c2558ea31
Sabageto <tanan0917025981@gmail.com>
2026-03-24 20:01:38 +0700

    Update BookTra and Trans

---
+++ src/main/java/vn/edu/fpt/comic/entity/BookTranslator.java
@@ -0,0 +1,33 @@
+package com.thunga.web.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "book_translator")
+public class BookTranslator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "translator_id", nullable = false)
+    private Translator translator;
+
+    @Column(name = "role", length = 50)
+    private String role;
+
+    @Column(name = "created_at")
+    private Date created_at;
+}
